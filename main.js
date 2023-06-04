b117ccd64b2b45db93fb25f20d12510500bf4326
LittleAyaan <125616620+LittleAyaan@users.noreply.github.com>
2023-06-04 19:49:58 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var canvas = new fabric.Canvas('myCanvas');
+var x= document.getElementById("myAudio");
+
+function new_image()
+{
+ fabric.Image.fromURL('BirthdayImage.jpg', function(Img) {
+  block_image_object = Img;
+ block_image_object.scaleToWidth(700);
+ block_image_object.scaleToWidth(510);
+ block_image_object.set({top:,left:0});
+  })
+}
+
+function playSound(){
+     x.play();
+}
